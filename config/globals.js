f02aee80d33de2631408e02e093481ebca1ad586
Marcelo Boeira <me@marceloboeira.com>
2014-06-29 15:07:43 -0300

    Revoming comments...

--- config/globals.js
+++ config/globals.js
@@ -1,13 +1,3 @@
-/**
- * Global Variable Configuration
- * (sails.config.globals)
- *
- * Configure which global variables which will be exposed
- * automatically by Sails.
- *
- * For more information on configuration, check out:
- * http://links.sailsjs.org/docs/config/globals
- */
 module.exports.globals = {
 	_: true,
 	async: true,
