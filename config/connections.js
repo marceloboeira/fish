7fb21cb663b2ab66c3b6d004d4113f4778732342
Marcelo Boeira <me@marceloboeira.com>
2014-07-12 15:05:19 -0300

    Revoming comments...

--- config/connections.js
+++ config/connections.js
@@ -1,17 +1,12 @@
-
-
 module.exports.connections = {
-
   development: {
     adapter: process.env.DEV_DB_ADAPTER ||  'sails-disk',
   },
-
   test: {
     adapter: process.env.TEST_DB_ADAPTER ||  'sails-disk',
     url: process.env.TEST_DB_URL || 'localhost',
     ssl: true
   },
-
   production: {
     adapter: process.env.PROD_DB_ADAPTER ||  'sails-disk',
     url: process.env.PROD_DB_URL || 'localhost',
