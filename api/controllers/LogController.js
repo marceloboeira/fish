8418f76f33af394b996da8018ecc4b4a04410523
Marcelo Boeira <marceloboeira@live.com>
2014-07-22 18:31:26 -0300

    Adding request and log  model/controller, ...

---
+++ api/controllers/LogController.js
@@ -0,0 +1,11 @@
+/**
+ * LogController
+ *
+ * @description :: Server-side logic for managing logs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Request.js
@@ -0,0 +1,31 @@
+/**
+* Request.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+  	//TODO - Relate to user*, Token.
+  	requestedAt: { 
+  		type: 'dateTime',
+  		required: true
+  	},
+  	time: {
+  		type: 'integer'
+  	},
+  	params: {
+  		type: 'text'
+  	},
+  	responseCode: {
+  		type: 'integer'
+  	},
+  	responseMessage: {
+  		type: 'text'
+  	}
+  }
+};
+
