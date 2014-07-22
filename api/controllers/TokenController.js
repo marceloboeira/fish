ddc54873b6c4f03633fc5ceb5fb5bc54fce927e9
Marcelo Boeira <marceloboeira@live.com>
2014-07-21 23:07:44 -0300

    Token and User Models...

---
+++ api/controllers/TokenController.js
@@ -0,0 +1,11 @@
+/**
+ * TokenController
+ *
+ * @description :: Server-side logic for managing tokens
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Token.js
@@ -0,0 +1,23 @@
+/**
+* Token.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+  	key: {
+  		type: 'string'
+  	},
+  	secret: {
+  		type: 'string'
+  	},
+  	owner:{
+  		model:'user'
+  	}
+  }
+};
+
