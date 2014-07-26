cdad7ee17870f1f12d350407108cf7e2ee09e9d9
Marcelo Boeira <marceloboeira@live.com>
2014-07-26 00:19:37 -0300

    Typos :ok_hand:

--- config/connections.js
+++ config/connections.js
@@ -3,17 +3,17 @@
 module.exports.connections = {
 
   development: {
-    adapter: procces.env.DEV_DB_ADAPTER || 'sails-disk'
+    adapter: process.env.DEV_DB_ADAPTER ||  'sails-disk',
   },
 
   test: {
-    adapter: procces.env.TEST_DB_ADAPTER || 'sails-mongo',
+    adapter: process.env.TEST_DB_ADAPTER ||  'sails-disk',
     url: process.env.TEST_DB_URL || 'localhost',
     ssl: true
   },
 
   production: {
-    adapter: procces.env.PROD_DB_ADAPTER || 'sails-mongo',
+    adapter: process.env.PROD_DB_ADAPTER ||  'sails-disk',
     url: process.env.PROD_DB_URL || 'localhost',
     ssl: true
   }
