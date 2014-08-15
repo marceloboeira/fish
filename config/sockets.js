83d1b7f4f53d79c1b0dd386dc2d5483d05f4931e
Marcelo Boeira <me@marceloboeira.com>
2014-08-15 14:58:38 -0300

    Revoming comments...

--- config/sockets.js
+++ config/sockets.js
@@ -21,56 +21,14 @@ module.exports.sockets = {
 
 
   authorization: false,
-
-  // Whether to run code which supports legacy usage for connected
-  // sockets running the v0.9 version of the socket client SDK (i.e. sails.io.js).
-  // Disabled in newly generated projects, but enabled as an implicit default (i.e.
-  // legacy usage/v0.9 clients be supported if this property is set to true, but also
-  // if it is removed from this configuration file or set to `undefined`)
   'backwardsCompatibilityFor0.9SocketClients': false,
-
-  // Whether to expose a 'get /__getcookie' route with CORS support
-  // that sets a cookie (this is used by the sails.io.js socket client
-  // to get access to a 3rd party cookie and to enable sessions).
-  //
-  // Warning: Currently in this scenario, CORS settings apply to interpreted
-  // requests sent via a socket.io connection that used this cookie to connect,
-  // even for non-browser clients! (e.g. iOS apps, toasters, node.js unit tests)
   grant3rdPartyCookie: true,
-
-  // Match string representing the origins that are allowed to connect to the Socket.IO server
   origins: '*:*',
-
-  // Should we use heartbeats to check the health of Socket.IO connections?
   heartbeats: true,
-
-  // When client closes connection, the # of seconds to wait before attempting a reconnect.
-  // This value is sent to the client after a successful handshake.
   'close timeout': 60,
-
-  // The # of seconds between heartbeats sent from the client to the server
-  // This value is sent to the client after a successful handshake.
   'heartbeat timeout': 60,
-
-  // The max # of seconds to wait for an expcted heartbeat before declaring the pipe broken
-  // This number should be less than the `heartbeat timeout`
   'heartbeat interval': 25,
-
-  // The maximum duration of one HTTP poll-
-  // if it exceeds this limit it will be closed.
   'polling duration': 20,
-
-  // Enable the flash policy server if the flashsocket transport is enabled
-  // 'flash policy server': true,
-
-  // By default the Socket.IO client will check port 10843 on your server
-  // to see if flashsocket connections are allowed.
-  // The Adobe Flash Player normally uses 843 as default port,
-  // but Socket.io defaults to a non root port (10843) by default
-  //
-  // If you are using a hosting provider that doesn't allow you to start servers
-  // other than on port 80 or the provided port, and you still want to support flashsockets
-  // you can set the `flash policy port` to -1
   'flash policy port': 10843,
 
   // Used by the HTTP transports. The Socket.IO server buffers HTTP request bodies up to this limit.
@@ -113,30 +71,10 @@ module.exports.sockets = {
   // This fixes issues with terminating the SSL in front of Node
   // and forcing location to think it's wss instead of ws.
   'match origin protocol': false,
-
-  // Direct access to the socket.io MQ store config
-  // The 'adapter' property is the preferred method
-  // (`undefined` indicates that Sails should defer to the 'adapter' config)
   store: undefined,
-
-  // A logger instance that is used to output log information.
-  // (`undefined` indicates deferment to the main Sails log config)
   logger: undefined,
-
-  // The amount of detail that the server should output to the logger.
-  // (`undefined` indicates deferment to the main Sails log config)
   'log level': undefined,
-
-  // Whether to color the log type when output to the logger.
-  // (`undefined` indicates deferment to the main Sails log config)
   'log colors': undefined,
-
-  // A Static instance that is used to serve the socket.io client and its dependencies.
-  // (`undefined` indicates use default)
   'static': undefined,
-
-  // The entry point where Socket.IO starts looking for incoming connections.
-  // This should be the same between the client and the server.
   resource: '/socket.io'
-
 };
